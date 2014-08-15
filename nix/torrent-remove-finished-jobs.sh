eaeeb6abfe66d953d2e1a592cb676c740bc7ba19
kot <samoylovnn@gmail.com>
2014-08-15 13:04:08 +0600

    Updated 'torrent-remove-finished-jobs.sh'

--- nix/torrent-remove-finished-jobs.sh
+++ nix/torrent-remove-finished-jobs.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-## @author    Samoylov Nikolay
-## @project   Remove downloaded torrents in Transmission torrent client
-## @copyright 2014 <samoylovnn@gmail.com>
-## @github    https://github.com/tarampampam/scripts/nix/
-## @version   0.1.0
+## @author      Samoylov Nikolay
+## @project     Remove downloaded torrents in Transmission torrent client
+## @copyright   2014 <samoylovnn@gmail.com>
+## @github      https://github.com/tarampampam/scripts/nix/
+## @version     0.1.1
 ##
-## @depends   bash, grep, awk, tr, transmission-remote
+## @depends     bash, grep, awk, tr, transmission-remote
 
 # Grep all lines with this substring:
-#   15   100%    2.3 GiB  Done         0.0     0.0   0.00  Idle         File.Name.Of.Movie.avi
-#                      ^^^^^^^
-FINISHED=`transmission-remote -l | grep "B  Done"`
-# echo "$FINISHED"
+#   15 100% 2.3 GiB Done 0.0 0.0 0.00 Idle File.Name.Of.Movie.avi
+#                 ^^^^^^
+FINISHED=$(transmission-remote -l | grep "B*Done" | tail -n +2)
+#echo "$FINISHED"
 
-# Get IDs list grom grepped list
-IDS=`echo "$FINISHED" | awk '{print $1}' | tr [:space:] ","`0
-# echo $IDS
+#Get IDs list grom grepped list
+IDS=$(echo "$FINISHED" | awk '{print $1}' | tr [:space:] ",")0
+echo " > Jobs IDs list to remove is: '$IDS'"
 
-# Say to transmission-remote for remove this torrents
-transmission-remote -t $IDS -r
+if [ ! "$IDS" = ",0" ]; then
+  # Say to transmission-remote for remove this torrents
+  echo -n " > 'transmission-remote' answer is: "; transmission-remote -t $IDS -r
+else
+  echo " > Nothing to remove"
+fi
