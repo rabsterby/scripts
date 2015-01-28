588564dd128e58633ceaa60167f6a49a82b39f35
kot <tarampampam@users.noreply.github.com>
2015-01-28 22:33:59 +0500

    Update block-sites.cmd

--- win/block-sites.cmd
+++ win/block-sites.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem Script created by Samoylov Nikolay < samoylovnn {d0g} gmail {d0t} com > # 2014
+rem Script created by Samoylov Nikolay <github.com/tarampampam> # 2014
 rem Version 0.1.1
 set BLOCK_SITE_REDIRECT=127.0.0.1
 set HOSTS_FILE="%SystemRoot%\system32\drivers\etc\hosts"
